40a67af593e82c3875f1907dd642404abe539b78
Denis_Yargin <denis.yargin@simbirsoft.com>
2023-04-19 01:35:30 +0300

    JUnit add

---
+++ src/test/java/com/demo/JUnitTests.java
@@ -0,0 +1,38 @@
+package com.demo;
+
+import org.junit.jupiter.api.*;
+
+public class JUnitTests {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("   beforeAll()");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("    afterAll()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("     beforeEach()");
+    }
+
+    @Test
+    void oneTest(){
+        Assertions.assertTrue(10>9);
+        System.out.println("       oneTest()");
+    }
+
+    @Test
+    void secondTest(){
+        Assertions.assertTrue(10>9);
+        System.out.println("       secondTest()");
+    }
+    @AfterEach
+    void afterEach(){
+        System.out.println("     afterEach");
+    }
+
+}
