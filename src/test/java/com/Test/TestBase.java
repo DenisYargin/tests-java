f365a5605b8dafe42a88771cf0642ef91f8afdc7
Denis_Yargin <deiderd@mail.ru>
2023-07-03 23:58:51 +0300

    refactor

--- src/test/java/com/Test/TestBase.java
+++ src/test/java/com/Test/TestBase.java
@@ -8,8 +8,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920x1080";
-         Configuration.pageLoadStrategy = "none";
-
+         Configuration.pageLoadStrategy = "none";пш
 //        Configuration.browser = "chrome";
 //        Configuration.timeout = 3000;
     }
