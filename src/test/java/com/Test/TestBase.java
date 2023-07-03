45795893e2c771159bc05f5fd3afe0e733625ad0
Denis_Yargin <deiderd@mail.ru>
2023-07-03 23:54:26 +0300

    test

--- src/test/java/com/Test/TestBase.java
+++ src/test/java/com/Test/TestBase.java
@@ -9,6 +9,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browserSize = "1920x1080";
          Configuration.pageLoadStrategy = "none";
+
 //        Configuration.browser = "chrome";
 //        Configuration.timeout = 3000;
     }
